38a56f5695b972c8c4f2fc02eb8607ace2ae1210
viktar_charnarutski <v.charnarutski@gmail.com>
2017-09-22 15:36:25 -0700

    Tests for the VacationPlan class.

---
+++ src/test/java/com/viktarx/model/VacationPlanTest.java
@@ -0,0 +1,68 @@
+package com.viktarx.model;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests for the VacationPlan class.
+ */
+public class VacationPlanTest {
+    @Test
+    public void possibleStartDate() throws Exception {
+        assertEquals(LocalDate.now().plusDays(5), new VacationPlan(LocalDate.now().plusDays(5), LocalDate.now().plusDays(10), 2,
+                "San Francisco, CA", "New York, NY").possibleStartDate());
+    }
+
+    @Test
+    public void possibleEndDate() throws Exception {
+        assertEquals(LocalDate.now().plusDays(10), new VacationPlan(LocalDate.now().plusDays(5), LocalDate.now().plusDays(10), 2,
+                "San Francisco, CA", "New York, NY").possibleEndDate());
+    }
+
+    @Test
+    public void minDurationInDays() throws Exception {
+        assertTrue(2 == new VacationPlan(LocalDate.now().plusDays(5), LocalDate.now().plusDays(10), 2,
+                "San Francisco, CA", "New York, NY").minDurationInDays());
+    }
+
+    @Test
+    public void departureCity() throws Exception {
+        assertEquals("San Francisco, CA", new VacationPlan(LocalDate.now().plusDays(5), LocalDate.now().plusDays(10), 2,
+                "San Francisco, CA", "New York, NY").departureCity());
+    }
+
+    @Test
+    public void destinationCity() throws Exception {
+        assertEquals("New York, NY", new VacationPlan(LocalDate.now().plusDays(5), LocalDate.now().plusDays(10), 2,
+                "San Francisco, CA", "New York, NY").destinationCity());
+    }
+
+    @Test
+    public void optionsForDateRanges() throws Exception {
+        VacationPlan vacationPlan = new VacationPlan(LocalDate.now().plusDays(5), LocalDate.now().plusDays(10), 2,
+                "San Francisco, CA", "New York, NY");
+
+        Set<DateRange> optionsForDateRanges = new HashSet<>();
+        optionsForDateRanges.add(new DateRange(LocalDate.now().plusDays(5), LocalDate.now().plusDays(7)));
+        optionsForDateRanges.add(new DateRange(LocalDate.now().plusDays(5), LocalDate.now().plusDays(8)));
+        optionsForDateRanges.add(new DateRange(LocalDate.now().plusDays(5), LocalDate.now().plusDays(9)));
+        optionsForDateRanges.add(new DateRange(LocalDate.now().plusDays(5), LocalDate.now().plusDays(10)));
+
+        optionsForDateRanges.add(new DateRange(LocalDate.now().plusDays(6), LocalDate.now().plusDays(8)));
+        optionsForDateRanges.add(new DateRange(LocalDate.now().plusDays(6), LocalDate.now().plusDays(9)));
+        optionsForDateRanges.add(new DateRange(LocalDate.now().plusDays(6), LocalDate.now().plusDays(10)));
+
+        optionsForDateRanges.add(new DateRange(LocalDate.now().plusDays(7), LocalDate.now().plusDays(9)));
+        optionsForDateRanges.add(new DateRange(LocalDate.now().plusDays(7), LocalDate.now().plusDays(10)));
+
+        optionsForDateRanges.add(new DateRange(LocalDate.now().plusDays(8), LocalDate.now().plusDays(10)));
+
+        assertEquals(optionsForDateRanges, vacationPlan.optionsForDateRanges());
+    }
+
+}
