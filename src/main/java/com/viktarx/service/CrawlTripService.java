ee8a2a2ce76018a202491b6ea930bad220ed9521
viktar_charnarutski <v.charnarutski@gmail.com>
2017-09-25 13:49:17 -0700

    Get request is added to the Crawl service.

--- src/main/java/com/viktarx/service/CrawlTripService.java
+++ src/main/java/com/viktarx/service/CrawlTripService.java
@@ -2,6 +2,9 @@ package com.viktarx.service;
 
 import com.viktarx.agent.TripOption;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.LocalDate;
 import java.util.Set;
 
@@ -9,13 +12,42 @@ import java.util.Set;
  * Structure implementation of a crawling service.
  */
 public abstract class CrawlTripService implements TripService {
+
     @Override
     public Set<TripOption> tripOptions(String departureCity, String destinationCity, LocalDate startDate, LocalDate endDate) {
-        return parsedTripOptionsFromWebPageContent(webPageContent(departureCity, destinationCity, startDate, endDate));
+        return parsedTripOptionsFromWebPageContent(responseForGet(urlParameters(departureCity, destinationCity, startDate, endDate)));
     }
 
-    abstract String webPageContent(String departureCity, String destinationCity, LocalDate startDate, LocalDate endDate);
+    abstract String urlParameters(String departureCity, String destinationCity, LocalDate startDate, LocalDate endDate);
 
     abstract Set<TripOption> parsedTripOptionsFromWebPageContent(String webPageContent);
 
+    abstract String serviceUrl();
+
+    private String responseForGet(String urlParameters) {
+        HttpURLConnection con;
+        StringBuilder response = new StringBuilder();
+        try {
+            URL targetUrl = new URL(serviceUrl() + urlParameters);
+            con = (HttpURLConnection) targetUrl.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+            } else {
+                throw new IllegalStateException(String.format("Failed to get response from %s. Response code: %d",
+                        serviceUrl(), responseCode));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response.toString();
+    }
 }
