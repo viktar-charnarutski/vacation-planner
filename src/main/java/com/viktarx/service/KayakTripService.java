ac2ed724bcfddc460501b8e182ffc12a308028b2
viktar_charnarutski <v.charnarutski@gmail.com>
2017-09-28 18:09:55 -0700

    Search id parsing is added.

--- src/main/java/com/viktarx/service/KayakTripService.java
+++ src/main/java/com/viktarx/service/KayakTripService.java
@@ -7,7 +7,10 @@ import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Crawling service implementation for the Kayak provider.
@@ -23,12 +26,18 @@ public class KayakTripService extends CrawlTripService {
     String rawDataFor(String departureCity, String destinationCity, LocalDate startDate, LocalDate endDate) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MMM-dd");
         String initResponse = responseForGetWithParams(paramsForGet(departureCity, destinationCity, startDate.format(formatter), endDate.format(formatter)));
-        String urlParameters = parsedSearchId(initResponse);
+        String urlParameters = parsedSearchIdFromRawPageContext(initResponse).orElse("");
         return responseForPostWithParams(paramsForPost(urlParameters));
     }
 
-    private String parsedSearchId(String rawPageContent) {
-        return "vtAsDFWpHp";
+    Optional<String> parsedSearchIdFromRawPageContext(String rawPageContent) {
+        Optional<String> result = Optional.empty();
+        Pattern pattern = Pattern.compile("(\\{\"searchId\":\"+)(\\w+)(\"})");
+        Matcher matcher = pattern.matcher(rawPageContent);
+        if (matcher.find()) {
+            result = Optional.of("searchId=" + matcher.group(2));
+        }
+        return result;
     }
 
     @Override
@@ -38,7 +47,7 @@ public class KayakTripService extends CrawlTripService {
 
     @Override
     String paramsForPost(String... params) {
-        return "searchId=vtAsDFWpHp&poll=true&pollNumber=0&applyFilters=true&filterState=&useViewStateFilterState=true&pageNumber=1&append=false&sortMode=rank&ascending=true&priceType=totaltaxes&requestReason=POLL&isSecondPhase=false&textAdPageLocations=bottom%2Cright&displayAdPageLocations=upper-right%2Cright&existingAds=false&ajaxts=1506481172251&scriptsMetadata=6D1f1E5CmDIIQQ2I1r1IDNHvgDwVR1uUFX1o1I1MxHuQj1%26QBBDOMa33I1C27B19B23oCSc1Pw%3D%3D&stylesMetadata=10gOG34QI1I14BQ10H1*e%26MDB1DgTgQUO%25QD1Cg1YEI1g1t1gMEBQBUN75C35ZILw2I1O64g1%3D%3D";
+        return params[0] + "&poll=true&pollNumber=0&applyFilters=true&filterState=&useViewStateFilterState=true&pageNumber=1&append=false&sortMode=rank&ascending=true&priceType=totaltaxes&requestReason=POLL&isSecondPhase=false&textAdPageLocations=bottom%2Cright&displayAdPageLocations=upper-right%2Cright&existingAds=false&ajaxts=1506481172251&scriptsMetadata=6D1f1E5CmDIIQQ2I1r1IDNHvgDwVR1uUFX1o1I1MxHuQj1%26QBBDOMa33I1C27B19B23oCSc1Pw%3D%3D&stylesMetadata=10gOG34QI1I14BQ10H1*e%26MDB1DgTgQUO%25QD1Cg1YEI1g1t1gMEBQBUN75C35ZILw2I1O64g1%3D%3D";
     }
 
     @Override
