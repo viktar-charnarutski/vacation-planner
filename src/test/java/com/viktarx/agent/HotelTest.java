cb4773946137773cfa682980ee05b7cba4a38009
viktar_charnarutski <v.charnarutski@gmail.com>
2017-10-08 15:06:48 -0700

    Removing unused imports.

--- src/test/java/com/viktarx/agent/HotelTest.java
+++ src/test/java/com/viktarx/agent/HotelTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.LocalDate;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
