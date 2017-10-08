6d6a7d70c42b7566aeb51d721147d0a9f77db8ec
viktar_charnarutski <v.charnarutski@gmail.com>
2017-10-08 15:06:24 -0700

    Tests for the Hotel class.

---
+++ src/test/java/com/viktarx/agent/HotelTest.java
@@ -0,0 +1,53 @@
+package com.viktarx.agent;
+
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests for the Hotel class.
+ */
+public class HotelTest {
+
+    private Hotel hotel;
+
+    public HotelTest() throws MalformedURLException {
+        hotel = new Hotel("San Souce", "A3, St-Mary, Jamaica", "All Inclusive", 4.0,
+                4.8, new URL("http://couples.com/resorts/sans-souci"));
+    }
+
+    @Test
+    public void name() throws Exception {
+        assertEquals("San Souce", hotel.name());
+    }
+
+    @Test
+    public void address() throws Exception {
+        assertEquals("A3, St-Mary, Jamaica", hotel.address());
+    }
+
+    @Test
+    public void type() throws Exception {
+        assertEquals("All Inclusive", hotel.type());
+    }
+
+    @Test
+    public void stars() throws Exception {
+        assertTrue(4.0 == hotel.stars());
+    }
+
+    @Test
+    public void rating() throws Exception {
+        assertTrue(4.8 == hotel.rating());
+    }
+
+    @Test
+    public void url() throws Exception {
+        assertEquals(new URL("http://couples.com/resorts/sans-souci"), hotel.url());
+    }
+}
